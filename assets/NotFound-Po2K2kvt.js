97d6b08a438f329f5af492685b2106fce9c2dd46
defnotsquishy <cheesehim21@gmail.com>
2026-08-25 16:24:51 +0100

    Updates

---
+++ assets/NotFound-Po2K2kvt.js
@@ -0,0 +1,6 @@
+import{j as t}from"./react-vendor-BwTK4uYV.js";import{c as o,B as a}from"./index-DWVFMuAU.js";import"./firebase-DqfwR_sZ.js";import"./motion-D_h6lSPk.js";/**
+ * @license lucide-react v0.460.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=o("House",[["path",{d:"M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8",key:"5wwlr5"}],["path",{d:"M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z",key:"1d0kgt"}]]),c="_page_f37og_1",n="_code_f37og_12",r="_text_f37og_22",e={page:c,code:n,text:r};function m(){return t.jsxs("div",{className:e.page,children:[t.jsx("h1",{className:e.code,children:"404"}),t.jsx("p",{className:e.text,children:"Page not found"}),t.jsx(a,{to:"/",variant:"primary",icon:s,children:"Go Home"})]})}export{m as default};
