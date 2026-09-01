0b273265da2a84c349c5cb9b5ce70535cea0100f
defnotsquishy <cheesehim21@gmail.com>
2026-09-01 20:39:55 +0100

    Deploy home-page language option

---
+++ assets/NotFound-Cbopb0BD.js
@@ -0,0 +1,6 @@
+import{j as e}from"./react-vendor-DSqCpQl4.js";import{c as s,P as o,B as c}from"./index-DHAAIN1J.js";import{t}from"./ThemedPage.module-Cj5UmXeu.js";import"./firebase-DXfntuoy.js";import"./motion-Bg65Emf5.js";/**
+ * @license lucide-react v0.460.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=s("House",[["path",{d:"M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8",key:"5wwlr5"}],["path",{d:"M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z",key:"1d0kgt"}]]),d="_page_3gqld_1",l="_code_3gqld_21",n="_text_3gqld_41",a={page:d,code:l,text:n};function g(){return e.jsxs(o,{className:t.pageShell,children:[e.jsx("div",{className:t.glow,"aria-hidden":"true"}),e.jsxs("div",{className:a.page,children:[e.jsx("h1",{className:a.code,children:"404"}),e.jsx("p",{className:a.text,children:"Page not found"}),e.jsx(c,{to:"/",variant:"primary",icon:r,children:"Go Home"})]})]})}export{g as default};
